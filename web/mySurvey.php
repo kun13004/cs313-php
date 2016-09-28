330143b9d0c58f72d827bff43245ec38ed3ffe4f
kun13004 <kun13004@byui.edu>
2016-09-28 17:46:35 -0600

    add survey

---
+++ web/mySurvey.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Confirmation Page</title>
+            </head>
+	<body >
+		
+		Name: <?php echo $_POST["name"]; ?><br>
+		
+		Lightsaber Color: <?php echo $_POST["importance"]; ?><br>
+		Lightsaber Color: <?php echo $_POST["personality"]; ?><br>
+		Lightsaber Color: <?php echo $_POST["roleModel"]; ?><br>
+		Lightsaber Color: <?php echo $_POST["class"]; ?><br>
+	</body>
+</html>
