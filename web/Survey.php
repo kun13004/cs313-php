031e3325b950254ff349f4ded3706fd8ae6e5480
kun13004 <kun13004@byui.edu>
2016-09-28 16:32:48 -0600

    stuff

--- web/Survey.php
+++ web/Survey.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>Confirmation Page</title>
-</head>
+            </head>
 	<body >
 		Name: <?php echo $_POST["name"]; ?><br>
 		Email: <?php echo $_POST["email"]; ?><br>
@@ -9,3 +9,25 @@
 		Comments: <?php echo $_POST["comment"]; ?><br>
 	</body>
 </html>
+
+
+
+
+<!--
+<html>
+	<head>
+		<title>Confirmation Page</title>
+</head>
+	<body >
+		Name: <?php //echo $_POST["name"]; ?><br>
+		Email: <?php //echo $_POST["email"]; ?><br>
+		Major: <?php //echo $_POST["major"]; ?><br>
+		Places: <? //foreach ($variable as $key => $value) {
+			//<?php echo $_POST["continent"]; 
+		} ?><br>
+		Comments: <?php //echo $_POST["comment"]; ?><br>
+	</body>
+</html>
+-->
+
+
