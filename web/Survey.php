e601c929c3e32d2b06820c16f691db1cdec3246d
kun13004 <kun13004@byui.edu>
2016-09-28 16:50:41 -0600

    more changes

--- web/Survey.php
+++ web/Survey.php
@@ -3,10 +3,15 @@
 		<title>Confirmation Page</title>
             </head>
 	<body >
+		
 		Name: <?php echo $_POST["name"]; ?><br>
 		Email: <?php echo $_POST["email"]; ?><br>
 		Major: <?php echo $_POST["major"]; ?><br>
-		Places: <?php isset($_POST["continent"]); ?><br>
+		Places: <?php $place = $_POST["continent"];
+		$N = count($place);
+		for($i = 0; $i < $N; $i++) {
+			echo htmlspecialchars($place[$i] ). " ";
+			} ?>
 		Comments: <?php echo $_POST["comment"]; ?><br>
 	</body>
 </html>
