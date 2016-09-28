2d215ab3a39e88bf3e1ef4597f1b2de3212eac82
kun13004 <kun13004@byui.edu>
2016-09-28 16:23:12 -0600

    stuff

--- web/Survey.php
+++ web/Survey.php
@@ -6,7 +6,6 @@
 		Name: <?php echo $_POST["name"]; ?><br>
 		Email: <?php echo $_POST["email"]; ?><br>
 		Major: <?php echo $_POST["major"]; ?><br>
-		<!--Places: <?php echo ?_POST["continent"]; ?><br> -->
 		Comments: <?php echo $_POST["comment"]; ?><br>
 	</body>
 </html>
