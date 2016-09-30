0c59d69f3ff50f3dc0638889f3d450f08b400188
kun13004 <kun13004@byui.edu>
2016-09-30 08:36:40 -0600

    changes

--- web/mySurvey.php
+++ web/mySurvey.php
@@ -5,8 +5,57 @@
 	<body >
 		
 		Name: <?php echo $_POST["name"]; ?><br>
+
+
+		<?php 
+			$importance = $_POST["importance"];
+			$personality = $_POST["personality"];
+			$roleModel = $_POST["roleModel"];
+			$class = $_POST["class"];
+			$blue = 0.4;
+			$green = 0.3;
+			$orange = 0.2;
+			$purple = 0.1;
+
+			function getResult($x) {
+				switch ($x) {
+				case 'blue':
+					$blue = $blue + 1;
+					break;
+				case 'green':
+					$green = $green + 1;
+					break;
+				case 'orange':
+					$orange = $orange + 1;
+					break;
+				default:
+					$purple = $purple + 1;
+					break;
+				}
+			}
+
+			function finalResult() {
+				if ($blue > $green && $blue > $orange && $blue > $purple) {
+					return $blue;
+				}
+				else if ($green > $blue && $green > $orange %% $green > $purple) {
+					return $green;
+				}
+				else if ($orange > $blue && $orange > $green && $orange > $purple) {
+					return $orange;
+				}
+				else
+					return $purple;
+			}
+
+			getResult($importance);
+			getResult($personality);
+			getResult($roleModel);
+			getResult($class);
+
+		?>
 		
-		Lightsaber Color: <?php echo $_POST["importance"]; ?><br>
+		Lightsaber Color: <?php echo finalResult(); ?><br>
 		Lightsaber Color: <?php echo $_POST["personality"]; ?><br>
 		Lightsaber Color: <?php echo $_POST["roleModel"]; ?><br>
 		Lightsaber Color: <?php echo $_POST["class"]; ?><br>
