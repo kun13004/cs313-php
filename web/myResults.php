c55e6cb88290fdfc1b477ac2d5b8088d0062980a
kun13004 <kun13004@byui.edu>
2016-09-30 21:49:55 -0600

    s

--- web/myResults.php
+++ web/myResults.php
@@ -20,7 +20,6 @@
 			$green = 0.3;
 			$orange = 0.2;
 			$purple = 0.1;
-			$sSheet = 1;
 
 			function getResult($x) {
 				global $blue, $green, $orange, $purple;
@@ -43,36 +42,33 @@
 			}
 			
 			function finalResult() {
+
+				global $blue, $green, $orange, $purple;
 				if ($blue < 1.0 && $green < 1.0 && $orange < 1.0 && $purple < 1.0) {
 					return;
 				}
-				global $blue, $green, $orange, $purple;
+
 				$myFile = fopen("mySurvey.txt", "a+") or die("Unable to open file!");
 
 				if ($blue > $green && $blue > $orange && $blue > $purple) {
 					$_SESSION["final"] = "Blue";
 					echo "<h1>" . $_SESSION["myName"] . ", your Lightsaber color is " . $_SESSION["final"] . "</h1><br>";
 					echo '<img src="http://i.imgur.com/6FfakhG.png" alt="blue lightsaber" />';
-
-					$sSheet = 1;
 				}
 				else if ($green > $blue && $green > $orange && $green > $purple) {
 					$_SESSION["final"] = "Green";
 					echo "<h1>" . $_SESSION["myName"] . ", your Lightsaber color is " . $_SESSION["final"] . "</h1><br>";
 					echo '<img src="http://i.imgur.com/xTHn4aL.png" alt="green lightsaber" />';
-					$sSheet = 2;
 				}
 				else if ($orange > $blue && $orange > $green && $orange > $purple) {
 					$_SESSION["final"] = "Orange";
 					echo "<h1>" . $_SESSION["myName"] . ", your Lightsaber color is " . $_SESSION["final"] . "</h1><br>";
 					echo '<img src="http://img12.deviantart.net/58e5/i/2015/075/c/6/beam_effect_dark_orange_by_kanbeikurodasamurai7-d8lx2g2.png" alt="orange lightsaber" />';
-					$sSheet = 3;
 				}
 				else if ($purple > $blue && $purple > $green && $purple > $orange) {
 					$_SESSION["final"] = "Purple";
 					echo "<h1>" . $_SESSION["myName"] . ", your Lightsaber color is " . $_SESSION["final"] . "</h1><br>";
 					echo '<img src="http://i.imgur.com/VNs2L1Y.png" alt="purple lightsaber" />';
-					$sSheet = 4;
 				}
 
 				fwrite($myFile, $_SESSION["myName"]);
